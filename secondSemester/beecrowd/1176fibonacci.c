5e715cd5e5d3bb7940579e166089a9065582072b
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2023-09-11 09:29:02 -0300

    exercicios

--- secondSemester/beecrowd/1176fibonacci.c
+++ secondSemester/beecrowd/1176fibonacci.c
@@ -5,9 +5,13 @@
 int main (){ 
   int repeticoes; 
   scanf("%i", &repeticoes); 
-  int saidas[61]; 
+  int saidas[60]; 
   int saida;
-  unsigned long long int impressao;
+  unsigned long long int impressao;  
+  unsigned long long int resultado[60]; 
+  resultado[0]=1; 
+  resultado[1]=1; 
+  resultado[2]=1;
   
   int i;
   for ( i = 0; i < repeticoes; i++)
@@ -15,16 +19,13 @@ int main (){
     scanf("\n%i", &saida);
     saidas[i]=saida;
   } 
-  unsigned long long int resultado[i]; 
-  resultado[0]=1; 
-  resultado[1]=1; 
-  resultado[2]=1;
+
   for(int j=3; j<60;j++){ 
     resultado[j]=resultado[j-1]+resultado[j-2];
   }
   
   
-  for(int y=0;y<i;y++){  
+  for(int y=0;y<repeticoes;y++){  
     impressao=resultado[saidas[y]];
     printf("\nFib(%i) = %lli",saidas[y], impressao);
   }
