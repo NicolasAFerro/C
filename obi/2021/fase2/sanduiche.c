75fba9fff6dc46608c00ab41009bb88f86b98ea1
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2023-10-10 12:37:13 -0300

    meio binário

---
+++ obi/2021/fase2/sanduiche.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+#include<math.h>
+
+int main(){ 
+  unsigned int letras, restricoes, soma=0;
+  int matriz[400][2];
+  scanf("%u %u", &letras, &restricoes);
+  //scanf("%u", &restricoes);
+  for(int i=0;i<restricoes;i++)
+    for(int j=0;j<2;j++)
+      scanf("%i ", matriz[i][j]); 
+  for (int i=0;i<letras;i++)
+    soma=soma+pow(2,i);
+  if(restricoes!=0) 
+    soma=soma-(restricoes+1);
+  printf("%u", soma);
+  return 0;
+}
